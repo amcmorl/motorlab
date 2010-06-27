4b77b12306e6807d5886844899c2bebb9b455888
Angus McMorland <amcmorl@gmail.com>
2010-06-27 09:38:19 -0400

    adding out_files, originally from tuning_change

---
+++ motorlab/out_files.py
@@ -0,0 +1,21 @@
+import os
+import time
+
+home = os.environ['HOME']
+pitt = '/files/pitt'
+
+project_dirs = {'networks' : home + pitt + '/networks',
+                'tuning_change' : home + pitt + '/tuning_change'}
+
+def make_today_dir(project='tuning_change'):
+    outputs = project_dirs[project]
+    today = outputs + '/' + time.strftime('%y%m%d')
+    if not os.path.exists(today):
+        os.mkdir(today)
+    return today
+
+def now_str():
+    return time.strftime('%H%M%S')
+
+def get_out_name(save_dir, what):
+    return save_dir + '/' + what + '_' + now_str()
