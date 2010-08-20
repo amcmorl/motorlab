f050f9c3d6d70dce455e6337e5dfa432d106f324
Angus McMorland <amcmorl@gmail.com>
2010-08-20 15:35:08 -0400

    load - made check_git_status less of a pain

--- motorlab/load.py
+++ motorlab/load.py
@@ -8,24 +8,23 @@ days_in_set = ['081309','081409','081709','081809','081909']
 data_locations = \
     { 'amcopti'  : "/home/amcmorl/files/pitt/frank/data/",
       'amcnote2' : "/home/amcmorl/files/pitt/frank/offline_sorted_multiday/" }
+uses_git = ['amcopti']
 
-def check_git_status(branch):
+def check_git_status(branch='any'):
     hostname = gethostname()
-    if hostname == 'amcopti':
-        # only amcopti uses a git-based data source
+    if hostname in uses_git:
         repo = Repo(data_locations[hostname])
-        if not repo.active_branch == branch:
-            raise EnvironmentError("Active branch in data git repository is "
-                   "%s, not %s as required." % (repo.active_branch, branch))
+        if branch == 'any':
+            warn("Active branch in data repo is %s" % (repo.active_branch))
+        elif not repo.active_branch == branch:
+            raise EnvironmentError("Active branch in data repo is"
+                   "%s; %s required." % (repo.active_branch, branch))
 
 def get_file_list(days=days_in_set, data_locs=data_locations,
-                  git_branch='offline_sorted_multiday'):
-    if git_branch == 'offline_sorted_multiday':
-        warn('default repository will be switched to master')
+                  git_branch='any'):
+    check_git_status(git_branch)
     data_dir = data_locs[gethostname()]
     file_pat = 'Frank.VR.*.CenterOut.mat'
-    if git_branch != None:
-        check_git_status(git_branch)
     files = []
     for day in days:
         files.extend(glob(data_dir + day + '/' + file_pat))
@@ -33,7 +32,7 @@ def get_file_list(days=days_in_set, data_locs=data_locations,
     return files
 
 def get_units_list(days=days_in_set, data_locs=data_locations,
-                   git_branch='master'):
+                   git_branch='any'):
     file0 = get_file_list(days=days, data_locs=data_locs,
                           git_branch=git_branch)[0]
     return list_units(file0)
@@ -49,4 +48,3 @@ def pick_units(units, cond='>100'):
         if eval(str(unit_num) + cond):
             out.append(unit)
     return out
-
